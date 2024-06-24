ed6554a1497f2f38722b1757172ea3c691b95ec6
BouchraMerabti <merabtibouchra@gmail.com>
2024-06-24 21:53:13 +0200

    first commit

---
+++ app/src/main/java/com/example/contactapp/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.contactapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvContact;
+    Button btnContact;
+
+    ImageView imPhone, imEmail, imLocal, imEmoji;
+
+    final int CREATE_CONTACT=  1;
+
+    String name = "", phone = "", email = "", location = "", mood = "";
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvContact = findViewById(R.id.tvContact);
+        btnContact = findViewById(R.id.btnContact);
+
+        imPhone = findViewById(R.id.imPhone);
+        imEmail = findViewById(R.id.imEmail);
+        imLocal = findViewById(R.id.imaLocal);
+        imEmoji = findViewById(R.id.imEmoji);
+
+        imPhone.setVisibility(View.GONE);
+        imEmail.setVisibility(View.GONE);
+        imLocal.setVisibility(View.GONE);
+        imEmoji.setVisibility(View.GONE);
+
+
+
+
+
+
+        btnContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, com.example.contactapp.SecondPage.class);
+                startActivityForResult(intent, CREATE_CONTACT);
+
+            }
+        });
+
+        imPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phone));
+                startActivity(intent);
+
+
+            }
+        });
+
+        imEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com" + email));
+                startActivity(intent);
+
+            }
+        });
+
+        imLocal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=155 Malmo , Sweden, Europe" + location));
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CREATE_CONTACT){
+            if (resultCode == RESULT_OK){
+                imPhone.setVisibility(View.VISIBLE);
+                imEmail.setVisibility(View.VISIBLE);
+                imLocal.setVisibility(View.VISIBLE);
+                imEmoji.setVisibility(View.VISIBLE);
+
+
+                name = data.getStringExtra("Name");
+                phone = data.getStringExtra("Phone");
+                email = data.getStringExtra("Email");
+                location = data.getStringExtra("Local");
+                mood = data.getStringExtra("mood");
+
+                if (mood.equals("smile")){
+                    imEmoji.setImageResource(R.drawable.smile);
+                } else if (mood.equals("neutral")) {
+                    imEmoji.setImageResource(R.drawable.neutral);
+                }  {
+                    imEmoji.setImageResource(R.drawable.sad);
+                }
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/contactapp/SecondPage.java
@@ -0,0 +1,71 @@
+package com.example.contactapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class SecondPage extends AppCompatActivity implements View.OnClickListener{
+
+    EditText etName, etPhone, etEmail, etLocation;
+
+    ImageView imSmile, imNeutral, imSad;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_page);
+
+        etName = findViewById(R.id.etName);
+        etPhone = findViewById(R.id.etPhone);
+        etEmail = findViewById(R.id.etEmail);
+        etLocation = findViewById(R.id.etLocation);
+
+        imSmile = findViewById(R.id.imSmile);
+        imNeutral = findViewById(R.id.imNeutral);
+        imSad = findViewById(R.id.imaSad);
+
+        imSmile.setOnClickListener(this);
+        imNeutral.setOnClickListener(this);
+        imSad.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (etName.getText().toString().isEmpty() || etPhone.getText().toString().isEmpty() ||
+        etEmail.getText().toString().isEmpty()|| etLocation.getText().toString().isEmpty()){
+            Toast.makeText(this,"Please enter all fields", Toast.LENGTH_SHORT).show();
+
+        } else{
+            Intent intent = new Intent();
+            intent.putExtra("Name", etName.getText().toString().trim());
+            intent.putExtra("Phone", etPhone.getText().toString().trim());
+            intent.putExtra("Email", etEmail.getText().toString().trim());
+            intent.putExtra("Local", etLocation.getText().toString().trim());
+
+            startActivity(intent);
+
+            if (view.getId() == R.id.imSmile){
+                intent.putExtra("mood", "smile");
+
+            } else if (view.getId() == R.id.imNeutral) {
+                intent.putExtra("mood", "neutral");
+
+            } else{
+
+                intent.putExtra("mood", "sad");
+
+            }
+            setResult(RESULT_OK, intent);
+            SecondPage.this.finish();
+        }
+
+    }
+}
